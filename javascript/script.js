582cc534dc73468e21f86310797588fcb03c6fcd
ritikxsharma <sharmaritik2002@gmail.com>
2022-06-25 03:02:06 +0530

    no images fixed

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,22 @@
+var menubar = document.querySelector('#menu-bar')
+var navbar = document.querySelector('.navbar')
+
+window.onscroll = () =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menubar.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+menubar.addEventListener('click', () => {
+    menubar.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+});
+
+
 function seeMoreLess() {
 
     var moreImgs = document.querySelector(".moreBox");
